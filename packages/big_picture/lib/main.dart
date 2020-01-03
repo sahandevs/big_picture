0ca6fb7a8f7ba0eb19e651b88693edbb5e2969ef
Sahandevs <sahandevs@gmail.com>
2020-01-03 11:42:32 +0330

    setup windows project

--- packages/big_picture/lib/main.dart
+++ packages/big_picture/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.android;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
